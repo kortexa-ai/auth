e3bdc021598f631b87efe13ea9f1f66b2da9528e
Franci Penov <francip@gmail.com>
2025-04-04 22:47:46 -0700

    Removing unnecessary components

--- src/auth/types.ts
+++ src/auth/types.ts
@@ -1,18 +1,22 @@
 // types.ts
 import type { Auth, AuthProvider, User } from 'firebase/auth';
-import { GithubAuthProvider, GoogleAuthProvider } from 'firebase/auth';
+import { GithubAuthProvider, GoogleAuthProvider, TwitterAuthProvider } from 'firebase/auth';
 
 export const googleAuthProvider = new GoogleAuthProvider();
 googleAuthProvider.setCustomParameters({ prompt: 'select_account' });
 
 export const githubAuthProvider = new GithubAuthProvider();
 
-export const SUPPORTED_PROVIDERS = ['google', 'github'] as const;
+export const twitterAuthProvider = new TwitterAuthProvider();
+
+export const SUPPORTED_PROVIDERS = ['google', 'github', 'twitter', 'apple'] as const;
 export type SupportedProviders = typeof SUPPORTED_PROVIDERS[number];
 
 export const AuthProviders = new Map<SupportedProviders | 'email', AuthProvider | null>([
     ['google', googleAuthProvider],
     ['github', githubAuthProvider],
+    ['twitter', twitterAuthProvider],
+    ['apple', null],
     ['email', null]
 ]);
 
@@ -26,6 +30,16 @@ export interface AuthState {
     mode: AuthMode;
 }
 
+/**
+ * Props for the AuthProvider component
+ * @param auth Firebase Auth instance
+ * @param loginRedirect Optional redirect URL to SSO provider for SSO login
+ * @param loginServer Optional login server URL for SSO token exchange
+ *
+ * loginRedirect and loginServer can't be used together
+ * If loginRedirect is provided, the component will act as SSO consumer
+ * If loginServer is provided, the component will act as SSO provider
+ */
 export interface AuthProviderProps {
     auth: Auth;
     loginRedirect?: string;
--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,5 @@ export { AuthProvider } from './auth/AuthProvider';
 export { useAuth } from './auth/hooks/useAuth';
 
 // UI Components
-export type { ContainerDimensionsProps } from './components/ContainerDimensions';
-export { ContainerDimensions } from './components/ContainerDimensions';
-export { SafeViewport } from './components/SafeViewport';
-
-export type { DebouncedInputProps } from './components/DebouncedInput';
-export { DebouncedInput } from './components/DebouncedInput';
-
 export { LoginForm } from './components/LoginForm';
 export { LoginView } from './components/LoginView';
