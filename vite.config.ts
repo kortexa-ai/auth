9f4f5ffb1d3c681337832be5fbb8f48e2cdb8de4
Franci Penov <francip@gmail.com>
2025-05-20 21:28:31 -0700

    Vite update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import path from "path"
-import dts from 'vite-plugin-dts'
-import react from "@vitejs/plugin-react-swc"
-import tailwindcss from '@tailwindcss/vite'
+import path from "path";
+import dts from "vite-plugin-dts";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [
@@ -15,44 +15,44 @@ export default defineConfig({
     ],
     optimizeDeps: {
         esbuildOptions: {
-            tsconfig: './tsconfig.app.json'
-        }
+            tsconfig: "./tsconfig.app.json",
+        },
     },
     resolve: {
         alias: {
-            "@": path.resolve(__dirname, './src'),
+            "@": path.resolve(__dirname, "./src"),
         },
     },
     build: {
-        outDir: './dist',
+        outDir: "./dist",
         chunkSizeWarningLimit: 2500,
         assetsInlineLimit: 0,
         lib: {
-            entry: path.resolve(__dirname, './src/index.ts'),
-            name: '@kortexa-ai/auth',
-            formats: ['es'],
+            entry: path.resolve(__dirname, "./src/index.ts"),
+            name: "@kortexa-ai/auth",
+            formats: ["es"],
         },
         rollupOptions: {
             external: [
-                'react',
-                'react/jsx-runtime',
-                'react-dom',
-                'firebase',
-                'firebase/app',
-                'firebase/auth',
+                "react",
+                "react/jsx-runtime",
+                "react-dom",
+                "firebase",
+                "firebase/app",
+                "firebase/auth",
             ],
             output: {
                 globals: {
-                    react: 'React',
-                    'react/jsx-runtime': 'jsxRuntime',
-                    'react-dom': 'ReactDOM',
-                    firebase: 'firebase',
-                    'firebase/app': 'firebase.app',
-                    'firebase/auth': 'firebase.auth',
+                    react: "React",
+                    "react/jsx-runtime": "jsxRuntime",
+                    "react-dom": "ReactDOM",
+                    firebase: "firebase",
+                    "firebase/app": "firebase.app",
+                    "firebase/auth": "firebase.auth",
                 },
             },
         },
         sourcemap: true,
-        emptyOutDir: true, // Usually good practice
+        emptyOutDir: true,
     },
 });
